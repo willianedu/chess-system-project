b295774e7d4c3a1ffc76e90fb9b65177a4f2758d
willianedu <willianeduardo9877@hotmail.com>
2023-03-01 10:40:42 -0300

    Placing pieces and UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position p = new Position(5,3);
-	    System.out.println(p);
+
+
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 }
